de15e2a0efb4a6ef581a61ea47c8c1816d6dec3c
Diana Frank <diana.frank22@gmail.com>
2016-11-21 23:21:52 -0500

    css changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
 "use strict";
 function toggleMenu(){
+	// add class to hide hamburger
   var x = document.getElementById("navigation")
-  console.log(x.className)
 	if (x.className === "nav"){
 		x.className += "responsive";
-		console.log(x.className)
 	}else{
 		x.className = "nav";
 	}
