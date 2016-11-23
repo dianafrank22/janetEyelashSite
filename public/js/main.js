dbeb4c10f474bc07496fd5ed6a95129dfe0f4ef0
Diana Frank <diana.frank22@gmail.com>
2016-11-23 16:29:24 -0500

    adds mobile nav css

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,17 @@
 "use strict";
 function toggleMenu(){
-	// add class to hide hamburger
   var x = document.getElementById("navigation")
+  var icon = document.getElementById("icon")
+  var classNameArray = document.getElementsByClassName("nav-div")
 	if (x.className === "nav"){
-		x.className += "responsive";
+		x.className += " responsive";
+		icon.className += " responsive";
+		for(var i = 0; i< classNameArray.length; i++){
+			var item = classNameArray[i]
+			item.className += " responsive"
+		}
 	}else{
 		x.className = "nav";
+		y.className = "nav-div"
 	}
 }
