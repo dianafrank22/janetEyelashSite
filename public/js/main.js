3c2d5ea9896c5c8553a78e241aa8aa5b93253560
Diana Frank <diana.frank22@gmail.com>
2016-11-28 13:48:35 -0500

    fixes function to add and remove class for hamburger menu, fixes styling for mobile menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 "use strict";
 function toggleMenu(){
   var x = document.getElementById("navigation")
-  var icon = document.getElementById("icon")
   var classNameArray = document.getElementsByClassName("nav-div")
-	if (x.className === "nav"){
+	if (x.className === "flex nav"){
 		x.className += " responsive";
-		icon.className += " responsive";
 		for(var i = 0; i< classNameArray.length; i++){
 			var item = classNameArray[i]
 			item.className += " responsive"
 		}
 	}else{
-		x.className = "nav";
-		y.className = "nav-div"
+		x.className = "flex nav";
+		for(var i = 0; i< classNameArray.length; i++){
+			var item = classNameArray[i]
+			item.className = ""
+			item.className = "nav-div box"
+		}
 	}
 }
